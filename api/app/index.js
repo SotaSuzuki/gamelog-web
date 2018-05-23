b472d0f49edab5e3b5741c07264b4bdb8c4b4e92
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-05-23 15:37:57 +0900

    Add nodemon to make development smooth

--- api/app/index.js
+++ api/app/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello world!'))
-
-app.listen(8081, () => {
-  console.log('listening on port 8081!')
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  next()
 })
+
+app.get('/', (req, res) => {
+  res.send('Hello Hoge!')
+})
+
+app.listen(8081)
+console.log('listening to port 8081')
