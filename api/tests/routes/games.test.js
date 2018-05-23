d7ee53a17423d57bbbfd9affe52bf7efebc96f09
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-05-23 18:28:34 +0900

    Update test

--- api/tests/routes/games.test.js
+++ api/tests/routes/games.test.js
@@ -2,9 +2,12 @@ const { app } = require('../../app')
 const request = require('supertest')
 
 describe('GET /:id', () => {
-  it('should be fine', (done) => {
+  it('should have title', (done) => {
     request(app)
       .get('/games/1')
+      .expect((res) => {
+        expect(res.body.game).toHaveProperty('title')
+      })
       .expect(200, done)
   })
 })
