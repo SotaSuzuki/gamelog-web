90d4735845ff267d71fbbea20b18c337e792eb72
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-05-24 17:37:23 +0900

    Add gameController

--- api/app/routes/games.js
+++ api/app/routes/games.js
@@ -1,59 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { connection } = require('./../db')
+const gameController = require('../controllers/gameController')
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-
-  connection.connect()
-
-  connection.query(`SELECT * FROM games WHERE id = ${id}`, (err, rows) => {
-    if (err) {
-      throw err
-    }
-    const game = rows[0]
-    const response = { game }
-    res.json(response)
-  })
-
-  connection.end()
-
-  // const response = {
-  //   game: {
-  //     title: 'Chrono Trigger',
-  //     description: '時空を越えるRPG',
-  //     releaseDate: '1995/3/11',
-  //     reviewCount: 2,
-  //     maker: {
-  //       name: 'スクウェア・エニックス',
-  //     },
-  //     genres: [
-  //       { id: 1, name: 'ロールプレイング' },
-  //     ],
-  //     platforms: [
-  //       { id: 1, name: 'SFC' },
-  //       { id: 2, name: 'DS' },
-  //     ],
-  //     reviews: [
-  //       {
-  //         body: 'マールは俺の嫁。',
-  //         author: {
-  //           id: '100',
-  //           name: 'hiropon',
-  //         },
-  //         publishedAt: '2001/3/9',
-  //       },
-  //       {
-  //         body: '人生で一番やりこんだゲーム。6周はした。',
-  //         author: {
-  //           id: '101',
-  //           name: 'pomodoroX',
-  //         },
-  //         publishedAt: '2000/11/1',
-  //       },
-  //     ],
-  //   },
-  // }
+  gameController.game(req, res)
 })
 
 module.exports = router
